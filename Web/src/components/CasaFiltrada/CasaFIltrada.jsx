1da0c101b16e038d6dfcdaffaefbd9b177e10fce
Gabriel Goncalves Albanez <00001089124053sp@aluno.educacao.sp.gov.br>
2023-08-02 12:21:21 -0300

    8att

--- Web/src/components/CasaFiltrada/CasaFIltrada.jsx
+++ Web/src/components/CasaFiltrada/CasaFIltrada.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AiOutlineUndo } from "react-icons/ai";
-import { IoMdPin } from "react-icons/io";
+import { AiTwotoneStar } from "react-icons/ai";
 
 export default function CasaFIltrada() {
   const { id } = useParams();
@@ -33,19 +33,32 @@ export default function CasaFIltrada() {
           <div className="flex items-center justify-center  flex-col h-[70vh] gap-2">
             <h1>Loading</h1>
             <button className="pl-4">
-              <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+              <svg
+                className="animate-spin h-5 w-5 mr-3 ..."
+                viewBox="0 0 24 24"
+              >
                 <AiOutlineUndo size={20} />
               </svg>
             </button>
           </div>
         </>
       ) : (
-        <>
-          <h1>Casa Filtrada</h1>
-          <ul>
-            <li key={dataById.id}>{dataById.local}</li>
-          </ul>
-        </>
+        <div className="w-screen h-[100vh] flex justify-center ">
+          <div className="border-[1px] h-[100vh] w-[60%] pt-10">
+            <div className="flex flex-col gap-4">
+              <h1 className="text-3xl ">{dataById.local}</h1>
+              <div className="flex items-center gap-3">
+                <div className="flex items-center gap-1">
+                <p><AiTwotoneStar size={20}/></p>
+                {/*esse numero de avalição vai ser passado pelo json*/}
+                <p>4,99</p>
+                </div>
+                 {/*esse numero de comentarios vai ser passado pelo json*/}
+                <p>109 comentarios</p>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
--- Web/src/components/Nabar.jsx
+++ Web/src/components/Nabar.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/imgs/Airbnb-Logo-768x279.png";
 import React, { useState } from "react";
 import { IoIosMenu } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 import LinkPersonalizado from "./Link";
 import { useLocation } from "react-router-dom";
 
@@ -20,63 +21,32 @@ export default function Nabar() {
           <img src={logo} alt="" height={130} width={130} />{" "}
         </LinkPersonalizado>
 
-        <div className="hidden md:block">
+        <div className="hidden sm:block">
           <div
-            className=" flex items-center justify-between w-[40vh] 
+            className=" flex items-center justify-between w-[35vh] 
       h-[4vh] px-4 shadow-md transition hover:shadow-xl border-[1px] rounded-full"
           >
-            <div>
-              <LinkPersonalizado caminho={"/produtos"}>
-                <p
-                  className={`
-          ${nomeRota.pathname === "/produtos" ? "text-rose-500" : ""}
-          `}
-                >
-                  Produtos
-                </p>
-              </LinkPersonalizado>
-            </div>
-            <div>|</div>
-
-            <div>
-              <LinkPersonalizado caminho={"/contatos"}>
-                <p
-                  className={`
-          ${nomeRota.pathname === "/contatos" ? "text-rose-500" : ""}
-          `}
-                >
-                  Contatos
-                </p>
-              </LinkPersonalizado>
-            </div>
-            <div>|</div>
-
-            <div>
-              <LinkPersonalizado caminho={"/account"}>
-                <p
-                  className={`
-          ${nomeRota.pathname === "/account" ? "text-rose-500" : ""}
-          `}
-                >
-                  Account
-                </p>
-              </LinkPersonalizado>
+            <div className="flex flex-row items-center justify-between w-[100%]">
+              <p className="">comece sua busca</p>
+              <div className="rounded-full flex items-center">
+                <CiSearch size={23} className="bg-white" />
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="hidden md:block">
+        <div className="">
           <div className="flex flex-row gap-10 items-center">
             <div
               onClick={openMeu}
-              className="rounded-full border-[1px] flex justify-center cursor-pointer transition shadow-md hover:shadow-xl items-center h-[5vh] w-[7vh]"
+              className="rounded-full border-[1px] relative flex justify-center cursor-pointer transition shadow-md hover:shadow-xl items-center h-[5vh] w-[7vh]"
             >
               <IoIosMenu size={25} />
               <FaUser size={25} />
             </div>
             {isOpen && (
               <>
-                <div className="bg-white  w-[150px] xl:left-[89%] md:left-[89%] sm:left-[89%]  absolute top-20 mr-10 flex flex-col items-center gap-3 shadow-lg transition hover:shadow-2xl rounded-md py-3 ">
+                <div className="bg-white  w-[150px] left-[88%]  absolute top-20 mr-10 flex flex-col items-center gap-3 shadow-lg transition hover:shadow-2xl rounded-md py-3 ">
                   <div className=" w-[100%] flex items-center justify-center cursor-pointer ">
                     Login
                   </div>
